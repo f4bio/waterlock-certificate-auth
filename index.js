847c7f95b1aba387d21827c285da471d199b377c
Fabio Tea <ft2011@gmail.com>
2015-10-10 22:01:06 +0200

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
- * waterlock-local-auth
- * Copyright(c) 2014 David Rivera <david.r.rivera193@gmail.com>
+ * waterlock-certificate-auth
+ * Copyright(c) 2015 Fabio Tea <ft2011@gmail.com>
+ * based on waterlock-local-auth from David Rivera <david.r.rivera193@gmail.com>
  * MIT Licensed
  */
-module.exports = require('./lib/waterlock-local-auth'); 
+module.exports = require('./lib/waterlock-certificate-auth');
