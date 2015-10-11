04deab1f40d4f9bf28b958c26f170c54273008ec
Fabio Tea <ft2011@gmail.com>
2015-10-11 18:41:37 +0200

    tests fixed

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,18 +1,17 @@
-var proxyquire =  require('proxyquire')
-  , should = require('should')
-  , path = require('path')
-  , waterlockPath = path.normalize(__dirname+'/waterlock.js')
+var proxyquire = require('proxyquire')
+	, should = require('should')
+	, path = require('path')
+	, waterlockPath = path.normalize(__dirname + '/waterlock.js')
 
 var pathStub = {
-  normalize: function(str){
-    return waterlockPath;
-  }
+	normalize: function (str) {
+		return waterlockPath;
+	}
 }
 
-exports.waterlock_local = proxyquire.noCallThru().load('../lib/waterlock-certificate-auth',
-  {
-    'path': pathStub
-  });
+exports.waterlock_certificate = proxyquire.noCallThru().load('../lib/waterlock-certificate-auth', {
+	'path': pathStub
+});
 
 exports.proxyquire = proxyquire;
 exports.should = should;
--- test/waterlock.js
+++ test/waterlock.js
@@ -1,4 +1,3 @@
-
 /**
  * waterlock
  *
@@ -9,38 +8,43 @@
  */
 module.exports.waterlock = {
 
-  // Base URL
-  //
-  // used by auth methods for callback URI's using oauth and for password
-  // reset links.
-  baseUrl: "http://localhost:1337",
+	// Base URL
+	//
+	// used by auth methods for callback URI's using oauth and for password
+	// reset links.
+	baseUrl: "http://localhost:1337",
 
-  // Auth Method(s)
-  //
-  // this can be a single string, an object, or an array of objects for your
-  // chosen auth method(s) you will need to see the individual module's README
-  // file for more information on the attributes necessary. This is an example
-  // of the local authentication method with password reset tokens disabled.
-  authMethod: [
-    {
-      name:"waterlock-certificate-auth"
-    }
-  ],
+	// Auth Method(s)
+	//
+	// this can be a single string, an object, or an array of objects for your
+	// chosen auth method(s) you will need to see the individual module's README
+	// file for more information on the attributes necessary. This is an example
+	// of the local authentication method with password reset tokens disabled.
+	// TODO: remove fingerprint (just for dev purposes)
+	authMethod: [
+		{
+			name: "waterlock-certificate-auth",
+			fingerprint: "12:34:56:78:90",
+			ca: {
+				file: "../certs/ca-crt.pem"
+			}
+		}
+	],
 
-  // JSON Web Tokens
-  //
-  // this provides waterlock with basic information to build your tokens,
-  // these tokens are used for authentication, password reset,
-  // and anything else you can imagine
-  jsonWebTokens:{
+	// JSON Web Tokens
+	//
+	// this provides waterlock with basic information to build your tokens,
+	// these tokens are used for authentication, password reset,
+	// and anything else you can imagine
+	jsonWebTokens: {
 
-    // CHANGE THIS SECRET
-    secret: "this is my secret",
-    expiry:{
-      unit: "days",
-      length: "7"
-    },
-    audience: "app name",
-    subject: "subject"
-  }
+		// CHANGE THIS SECRET
+		secret: "this is my secret",
+		expiry: {
+			unit: "days",
+			length: "7"
+		},
+		audience: "app name",
+		subject: "subject"
+	}
 }
