31b3bcf29faf24fd661c74ed2d1cc7166cdb376b
Fabio Tea <ft2011@gmail.com>
2015-10-19 18:49:11 +0200

    save

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
  * Copyright(c) 2015 Fabio Tea <ft2011@gmail.com>
  * based on waterlock-local-auth from David Rivera <david.r.rivera193@gmail.com> - THX!
  */
-module.exports = require('./lib/waterlock-certificate-auth');
+module.exports = require("./lib/waterlock-certificate-auth");
